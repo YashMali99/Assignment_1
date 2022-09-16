b2618b1daae335cfcb746995a8b77aa6edb9a8ed
YashMali99 <112956704+YashMali99@users.noreply.github.com>
2022-09-16 19:54:03 +0530

    Answer of question no. 3

--- Assignment_1_Q_3.py
+++ Assignment_1_Q_3.py
@@ -1,5 +1,5 @@
 
-# Q-3) Write a Python program to count the number of even and odd numbers from a series of numbers
+# Q-3) Write a Python program to count the number of even and odd numbers from a series of numbers 
 # Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
 # Expected Output :
 # Number of even numbers : 4
